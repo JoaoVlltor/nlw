3d21d836e7da8038c7ec2a74164ea88cb27625c8
Joao Vitor <novoaff@gmail.com>
2020-10-17 23:24:42 -0300

    Nice

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const pages = require('./pages.js');
+
+const server = express()
+server
+//utilizar body do req
+
+.use(express.urlencoded({ extended: true }))
+
+.use(express.static('public'))
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'hbs')
+
+server.get('/', pages.index)
+server.get('/orphanage', pages.orphanage)
+server.get('/orphanages', pages.orphanages)
+server.get('/create-orphanage', pages.createOrphanage)
+.post('/save-orphanage', pages.saveOrphanage)
+
+server.listen(5500, () => {
+    console.log ('Server started')
+})
